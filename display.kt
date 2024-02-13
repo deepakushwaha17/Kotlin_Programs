bf839acfd4d65b135f14fe7a0b57bba347e104c5
deepakushwaha17 <deepakushwaha155@gmail.com>
2024-02-13 16:10:31 +0530

    float num

---
+++ display.kt
@@ -0,0 +1,10 @@
+import java.awt.geom.Arc2D
+
+//Program to take float input and print it.
+fun main()
+{
+    val num1 = 5.0F
+    var num2 = 50.034F
+    println("Float number num1 value is " +num1)
+    println("Float number num2 value is " +num2)
+}
